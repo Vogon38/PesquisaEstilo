45fbe854e8205fa2bf9e8267d9ca2abb1176e1be
Vogon38 <luana@thecyborg.xyz>
2022-12-26 04:03:39 -0300

    Ajustes gerais

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-/* eslint-disable no-dupe-keys */
-import { useCallback, useState, useRef } from 'react';
 import './App.css';
-import { surveyJson } from './json.js';
-
 import 'survey-core/modern.min.css';
+import { React, useCallback, useState, useRef } from 'react';
+import { surveyJson } from './json.js';
 import { StylesManager, Model } from 'survey-core';
 import { Survey } from 'survey-react-ui';
 
 StylesManager.applyTheme("modern");
 
 function App() {
-  // useRef enables the Model object to persist between state changes
   const survey = useRef(new Model(surveyJson)).current;
   const [surveyResults, setSurveyResults] = useState("");
   const [isSurveyCompleted, setIsSurveyCompleted] = useState(false);
-
   const displayResults = useCallback((sender) => {
     setSurveyResults(JSON.stringify(sender.data, null, 4));
     setIsSurveyCompleted(true);
@@ -24,7 +20,7 @@ function App() {
 
   return (
     <>
-      <Survey model={survey} />
+      <Survey model={survey} id="surveyContainer" />
       {isSurveyCompleted && (
         <>
           <p>Result JSON:</p>
