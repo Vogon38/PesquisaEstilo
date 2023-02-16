aa9edc1e7f7aff8961dc4aebd22bb5a08f606af5
Vogon38 <luana@thecyborg.xyz>
2023-02-16 02:46:41 -0300

    creating chart results

--- src/App.js
+++ src/App.js
@@ -1,36 +1,85 @@
 import './App.css';
-import 'survey-core/modern.min.css';
-import { React, useCallback, useState, useRef } from 'react';
-import { surveyJson } from './json.js';
+import { useState, useCallback } from 'react';
 import { StylesManager, Model } from 'survey-core';
 import { Survey } from 'survey-react-ui';
+import { Doughnut } from 'react-chartjs-2';
+import { surveyJson } from './json';
 
 StylesManager.applyTheme("modern");
 
 function App() {
-  const survey = useRef(new Model(surveyJson)).current;
-  const [surveyResults, setSurveyResults] = useState("");
-  const [isSurveyCompleted, setIsSurveyCompleted] = useState(false);
+  const [surveyResults, setSurveyResults] = useState(null);
+  const [chartData, setChartData] = useState(null);
+
+  const chartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    legend: {
+      position: 'bottom'
+    }
+  };
+
+  const calculateChartData = (results) => {
+    const data = {
+      "1": 0,
+      "2": 0,
+      "3": 0,
+      "4": 0,
+      "5": 0,
+      "6": 0,
+      "7": 0
+    };
+    for (const key in results) {
+      const answer = results[key];
+      data[answer] += 1;
+    }
+    return {
+      labels: Object.keys(data),
+      datasets: [
+        {
+          data: Object.values(data),
+          backgroundColor: [
+            '#36A2EB',
+            '#FF6384',
+            '#FFCE56',
+            '#8e5ea2',
+            '#3cba9f'
+          ]
+        }
+      ]
+    };
+  };
+
   const displayResults = useCallback((sender) => {
-    setSurveyResults(JSON.stringify(sender.data, null, 4));
-    setIsSurveyCompleted(true);
+    setSurveyResults(sender.data);
+    setChartData(calculateChartData(sender.data));
   }, []);
 
-  survey.onComplete.add(displayResults);
+  const renderChart = () => {
+    if (chartData) {
+      return (
+        <div style={{ width: '300px', height: '300px' }}>
+          <Doughnut data={chartData} options={chartOptions} />
+        </div>
+      );
+    } else {
+      return null;
+    }
+  };
 
   return (
-    <>
-      <Survey model={survey} id="surveyContainer" />
-      {isSurveyCompleted && (
-        <>
-          <p>Result JSON:</p>
-          <code style={{ whiteSpace: 'pre' }}>
-            {surveyResults}
-          </code>
-        </>
-      )
-      }
-    </>
+    <div className="App">
+      <header className="App-header">
+        <Survey
+          model={new Model(surveyJson)}
+          onComplete={displayResults}
+        />
+        {surveyResults && (
+          <pre>{JSON.stringify(surveyResults, null, 2)}</pre>
+        )}
+        {renderChart()}
+      </header>
+    </div>
   );
 }
 
